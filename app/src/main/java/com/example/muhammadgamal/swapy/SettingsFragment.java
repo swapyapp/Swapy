45af51ae2691b10256f4db3220de788efb0bcd48
swapyapp <43712240+swapyapp@users.noreply.github.com>
2018-10-09 00:07:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/muhammadgamal/swapy/SettingsFragment.java
@@ -0,0 +1,19 @@
+package com.example.muhammadgamal.swapy;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        getActivity().setTitle("Settings");
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
